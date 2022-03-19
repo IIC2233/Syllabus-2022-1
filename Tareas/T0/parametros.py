d63ecf2f23f3fd1baba6b4b2a4eb283c156fe285
matiasmasjuan <matas.masjuan@uc.cl>
2022-03-19 11:56:02 -0300

    T0 📬️🚐️

---
+++ Tareas/T0/parametros.py
@@ -0,0 +1,11 @@
+# Mínimo de caracteres que debe tener el nombre de un nuevo usuario
+MIN_CARACTERES = 5
+
+# Contraseña del administrador
+CONTRASENA_ADMIN = "DCCorreos"
+
+# Peso máximo de una encomienda
+MAX_PESO = 50
+
+# Largo mínimo de la contraseña
+LARGO_CONTRASENA = 6
