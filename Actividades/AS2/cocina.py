572ebde167ce22cd24e4bf00ded7edb954b1c6f6
Ian Basly <igbasly@uc.cl>
2022-04-21 14:20:32 -0400

    AS2 publicada :star:

---
+++ Actividades/AS2/cocina.py
@@ -0,0 +1,36 @@
+from collections import deque
+from entidades import Cocinero, Mesero
+from time import sleep
+from random import randint
+
+
+class Cocina:
+
+    def __init__(self, bodega, recetas):
+        super().__init__()
+        self.cola_pedidos = deque()
+        self.cola_pedidos_listos = deque()
+        self.cocineros = []
+        self.meseros = []
+        self.bodega = bodega
+        self.recetas = recetas
+        self.abierta = True
+
+    def initialize_threads(self):
+        # Completar
+        pass
+
+    def asignar_cocinero(self):
+        # Completar
+        pass
+
+    def asignar_mesero(self):
+        # Completar
+        pass
+
+    def finalizar_jornada_laboral(self):
+        for mesero in self.meseros:
+            mesero.trabajando = False
+
+        for cocinero in self.cocineros:
+            cocinero.trabajando = False
